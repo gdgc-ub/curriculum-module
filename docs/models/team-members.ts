fcc15a9ee60e82ddca3dd8f60c914430fc6e3290
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-11-04 13:19:23 +0700

    `feat(core)`: Remove Mock Data in Team Page

--- docs/models/team-members.ts
+++ docs/models/team-members.ts
@@ -1,48 +1,70 @@
 // For Future curriculum, please don't erase previous curriculum
 export const members = [
     {
-      avatar: 'https://www.github.com/yyx990803.png',
-      name: 'Evan You',
-      title: 'Creator',
+      avatar: '../public/assets/team-member/faiz.png',
+      name: 'Ahnad Faiz Agustianto',
+      title: 'Curriculum Lead',
       links: [
-        { icon: 'github', link: 'https://github.com/yyx990803' },
-        { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/ahmad-faiz-agustianto/'},
+        { icon: 'instagram', link: 'https://www.instagram.com/ahmdpais_' },
+        { icon: 'github', link: 'https://github.com/Mfaki' }
       ]
     },
     {
-        avatar: 'https://www.github.com/yyx990803.png',
-        name: 'Evan You',
-        title: 'Creator',
-        links: [
-          { icon: 'github', link: 'https://github.com/yyx990803' },
-          { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
-        ]
+      avatar: '../public/assets/team-member/deta.png',
+      name: 'Bernadeta Regina Kusumawardhani',
+      title: 'Product Management',
+      links: [
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/bernadeta-regina-kusumawardhani/'},
+        { icon: 'instagram', link: 'https://www.instagram.com/bernadetaregina/' },
+      ]
     },
     {
-        avatar: 'https://www.github.com/yyx990803.png',
-        name: 'Evan You',
-        title: 'Creator',
-        links: [
-          { icon: 'github', link: 'https://github.com/yyx990803' },
-          { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
-        ]
+      avatar: '../public/assets/team-member/rara.png',
+      name: 'Nareswara Lintang Kirana',
+      title: 'UI/UX',
+      links: [
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/nareswaralintangkirana/'},
+        { icon: 'instagram', link: 'https://www.instagram.com/rarakiraana/' },
+      ]
     },
     {
-        avatar: 'https://www.github.com/yyx990803.png',
-        name: 'Evan You',
-        title: 'Creator',
-        links: [
-          { icon: 'github', link: 'https://github.com/yyx990803' },
-          { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
-        ]
+      avatar: '../public/assets/team-member/indra.png',
+      name: 'Gede Indra Adi Brata',
+      title: 'Cloud Development',
+      links: [
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/indrabrata/'},
+        { icon: 'instagram', link: 'https://www.instagram.com/lndra.a/' },
+        { icon: 'github', link: 'https://github.com/Ndraaa15' }
+      ]
     },
     {
-        avatar: 'https://www.github.com/yyx990803.png',
-        name: 'Evan You',
-        title: 'Creator',
-        links: [
-          { icon: 'github', link: 'https://github.com/yyx990803' },
-          { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
-        ]
+      avatar: '../public/assets/team-member/elgin.png',
+      name: 'Elgin Brian Wahyu Bramadhika',
+      title: 'Android Development',
+      links: [
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/elginbrian/'},
+        { icon: 'instagram', link: 'https://www.instagram.com/_elginbrian/' },
+        { icon: 'github', link: 'https://github.com/elginbrian' }
+      ]
+    },
+    {
+      avatar: '../public/assets/team-member/nickolas.png',
+      name: 'Nickolas Budiyono',
+      title: 'Web Development',
+      links: [
+        { icon: 'instagram', link: 'https://www.instagram.com/nickolaswewee/' },
+        { icon: 'github', link: 'https://github.com/nickolasww' }
+      ]
+    },
+    {
+      avatar: '../public/assets/team-member/rafly.png',
+      name: 'Muhammad Rafly Ash Shiddiqi',
+      title: 'Machine Learning',
+      links: [
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/einrafh/'},
+        { icon: 'instagram', link: 'https://www.instagram.com/einrafh/' },
+        { icon: 'github', link: 'https://github.com/Einrafh' }
+      ]
     },
 ];
