2b33957fe8cbeaf3d421e75e6fca44dc9faf0ad7
devanfer <ferrelvsc20@gmail.com>
2024-11-18 17:11:33 +0700

    `change(cloud):` sidebar path

--- docs/models/sidebar/cloud-development/CloudSidebar.ts
+++ docs/models/sidebar/cloud-development/CloudSidebar.ts
@@ -6,25 +6,28 @@ export const CloudSidebar = [
     {
         text: 'Introduction with GCP',
         items: [
-          { text: "What is GCP", link: `${CLOUD_NOTE_PATH}/intro-with-gcp/what-is-gcp` },
-          { text: "GCP Products", link: `${CLOUD_NOTE_PATH}/intro-with-gcp/gcp-products`,},
-          { text: "Interacting with GCP", link: `${CLOUD_NOTE_PATH}/intro-with-gcp/interacting-with-gcp`}
+          { text: "What is GCP", link: `${CLOUD_NOTE_PATH}/2-intro-gcp/2.1-what-is-gcp` },
+          { text: "GCP Products", link: `${CLOUD_NOTE_PATH}/2-intro-gcp/2.2-gcp-products`,},
+          { text: "Interacting with GCP", link: `${CLOUD_NOTE_PATH}/2-intro-gcp/2.3-interacting-with-gcp`}
         ]
     },
     {
       text: 'Virtualization vs Containerization',
       items: [
-        { text: "Virtualization", link: `${CLOUD_NOTE_PATH}/v-vs-c/virtualization` },
-        { text: "Containerization", link: `${CLOUD_NOTE_PATH}/v-vs-c/containerization` },
-        { text: "Key Differences", link: `${CLOUD_NOTE_PATH}/v-vs-c/key-differences` }
+        { text: "Virtualization", link: `${CLOUD_NOTE_PATH}/3-virtualization-vs-containerization/3.1-containerization` },
+        { text: "Containerization", link: `${CLOUD_NOTE_PATH}/3-virtualization-vs-containerization/3.2-virtualization` },
+        { text: "Key Differences", link: `${CLOUD_NOTE_PATH}/3-virtualization-vs-containerization/3.3-differences-virtualization-containerization` }
       ]
     },
     {
       text: 'Create Your First VM',
       items: [
-        { text: "Introduction with Virtual Box", link: `${CLOUD_NOTE_PATH}/your-first-vm/intro-vbox` },
-        { text: "Tools Installation", link: `${CLOUD_NOTE_PATH}/your-first-vm/tools-installation` },
-        { text: "Create Virtual Machine", link: `${CLOUD_NOTE_PATH}/your-first-vm/create-vm` },
+        { text: "Introduction with Virtual Box", link: `${CLOUD_NOTE_PATH}/4-virtual-machine/4.1-intro-virtual-box` },
+        { text: "Tools Installation", link: `${CLOUD_NOTE_PATH}/4-virtual-machine/4.2-tools-installation` },
+        { text: "Create Virtual Machine", link: `${CLOUD_NOTE_PATH}/4-virtual-machine/4.3-create-virtual-machine` },
       ]
     },
+    {
+      text: "Containerization"
+    }
 ]  
